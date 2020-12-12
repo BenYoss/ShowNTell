c66c53914126e2d75990709f327f1a8e3644dc4d
benyoss <benyoss4@gmail.com>
2020-12-12 17:29:18 -0600

    (fix) for rebase merge

--- client/src/components/HomeFeed/homeFeed.jsx
+++ client/src/components/HomeFeed/homeFeed.jsx
@@ -6,23 +6,45 @@ import FeedItem from './feedItem.jsx';
 
 import './homefeed.css';
 
-const HomeFeed = ({ posts, handleUserClick, user, setPosts }) => (
-  <div>
-    <div className="home-title"> Home feed</div>
-    <div className="home-feed-container">
-      {posts
-        ? posts.map((post, i) => (
-          <FeedItem
-            handleUserClick={handleUserClick}
-            post={post}
-            setPosts={setPosts}
-            key={post + i}
-            user={user}
-          />
-        ))
-        : null}
+const HomeFeed = ({ posts, handleUserClick, user, setPosts }) => {
+  const [selectPost, setSelectPost] = useState('');
+
+  const useStyles = makeStyles((theme) => ({
+    hide: {
+      display: 'none',
+    },
+    drawer: {
+      width: 1000,
+      flexShrink: 0,
+      whiteSpace: 'nowrap',
+    },
+    drawerOpen: {
+      width: 1000,
+      transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    },
+  }));
+
+  return (
+    <div>
+      <div className="home-title"> Home feed</div>
+      <div className="home-feed-container">
+        {posts
+          ? posts.map((post, i) => (
+            <FeedItem
+              handleUserClick={handleUserClick}
+              post={post}
+              setPosts={setPosts}
+              key={post + i}
+              user={user}
+            />
+          ))
+          : null}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default HomeFeed;
